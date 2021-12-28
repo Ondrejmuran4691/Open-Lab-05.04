090bb642bfc5f90ad8117dcd6a7b3baaf6a93e89
Ondrejmuran4691 <58992354+Ondrejmuran4691@users.noreply.github.com>
2021-12-28 19:54:23 +0100

    removes special characters from string

---
+++ Open-Lab-05.04/Open-Lab-05.04/StringTools.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+namespace Open_Lab_05._04
+{
+    public class StringTools
+    {
+        public string RemoveSpecialCharacters(string original)
+        {
+            original = original.Replace("!", "");
+            original = original.Replace("%", "");
+            original = original.Replace("$", "");
+            original = original.Replace("(", "");
+            original = original.Replace(")", "");
+            original = original.Replace(".", "");
+            original = original.Replace("#", "");
+            original = original.Replace("&", "");
+            original = original.Replace("?", "");
+            original = original.Replace("*", "");
+            original = original.Replace("<", "");
+            original = original.Replace(">", "");
+            original = original.Replace("/", "");
+            original = original.Replace("@", "");
+            original = original.Replace("`", "");
+            original = original.Replace("^", "");
+            original = original.Replace(";", "");
+            original = original.Replace(":", "");
+            original = original.Replace("[", "");
+            original = original.Replace("]", "");
+            original = original.Replace("=", "");
+            original = original.Replace(",", "");
+            original = original.Replace("'", "");
+            original = original.Replace("+", "");
+            original = original.Replace('"'.ToString(), "");
+
+
+            original = original.Replace("\\", "");
+
+            return original;
+        }
+    }
+}
